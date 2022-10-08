d500a20d7fe050b9f2b617a5fe6ba0d45b656930
Perez Romero <operezr@minsait.com>
2022-10-08 10:57:24 +0200

    feat(autoclicker-home) implement layout and basic styles

--- packages/autoclicker-home/src/AutoclickerHome.js
+++ packages/autoclicker-home/src/AutoclickerHome.js
@@ -2,7 +2,58 @@ import { html, css, LitElement } from "lit";
 
 export class AutoclickerHome extends LitElement {
   static get styles() {
-    return css``;
+    return css`
+      :host {
+        width: 100%;
+        height: 100%;
+        margin-top: 10rem;
+
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: flex-start;
+        gap: 2rem;
+      }
+
+      h2 {
+        font-size: 2.5rem;
+        font-weight: 100;
+        color: var(--home-title-color, white);
+      }
+
+      form {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+        gap: 2rem;
+        width: 60vw;
+      }
+
+      .field {
+        background-color: transparent;
+        color: var(--home-input-color, white);
+        width: 100%;
+        font-size: 2rem;
+        padding: 1.5rem;
+        border-radius: 0.7rem;
+        border: 1px solid var(--home-input-color, white);
+      }
+
+      .field::placeholder {
+        color: var(--home-placeholder-color, #ccc);
+      }
+
+      .button {
+        font-size: 1.5rem;
+        padding: 1.5rem 5rem;
+        border-radius: 0.7rem;
+        text-transform: uppercase;
+        color: var(--home-button-text-color, #000000);
+        border: 1px solid var(--home-button-border-color, #000000);
+        background-color: var(--home-button-background-color, #86edff);
+      }
+    `;
   }
 
   static get properties() {
@@ -14,8 +65,19 @@ export class AutoclickerHome extends LitElement {
   }
 
   render() {
-    return html` <h2>Hi from home view</h2>
-      <button @click=${this.navigateToGame}>Start</button>`;
+    return html`<h2>Create new player</h2>
+      <form>
+        <input
+          class="field"
+          placeholder="Name"
+          maxlength="7"
+        ></input>
+        <button
+          class="button"
+          @click=${this.navigateToGame}
+          >Start</button
+        >
+      </form> `;
   }
 
   navigateToGame() {
