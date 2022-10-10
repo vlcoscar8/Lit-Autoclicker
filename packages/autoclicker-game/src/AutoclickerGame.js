86f15bfade8262ca54576027f509fe752e570b42
Perez Romero <operezr@minsait.com>
2022-10-10 17:36:39 +0200

    feat(autoclicker-game): update styles and add icons and effects

--- packages/autoclicker-game/src/AutoclickerGame.js
+++ packages/autoclicker-game/src/AutoclickerGame.js
@@ -66,6 +66,9 @@ export class AutoclickerGame extends LitElement {
         padding: 1.5rem 9rem;
         border-radius: 0.7rem;
         text-transform: uppercase;
+        color: var(--home-button-text-color, #000000);
+        border: 1px solid var(--home-button-border-color, #000000);
+        background-color: var(--home-button-background-color, #86edff);
       }
 
       .disabled {
@@ -77,7 +80,74 @@ export class AutoclickerGame extends LitElement {
       .active {
         color: var(--home-button-text-color, #000000);
         border: 1px solid var(--home-button-border-color, #000000);
-        background-color: var(--home-button-background-color, #86edff);
+        background-color: var(--game-button-active-background-color, #86edff);
+      }
+
+      .energy {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        box-sizing: border-box;
+        gap: 1rem;
+        width: 80vw;
+        height: 6rem;
+      }
+
+      .energy img {
+        width: 3rem;
+      }
+
+      :host([boosted]) .energy img {
+        animation: grow 0.5s ease;
+      }
+
+      .planet {
+        width: 7rem;
+      }
+
+      @keyframes grow {
+        0% {
+          width: 3rem;
+        }
+
+        50% {
+          width: 4rem;
+        }
+      }
+
+      .ripple {
+        --ripple-background: #992d2d4c;
+        --ripple-opacity: 0.1;
+        --ripple-duration: 600ms;
+        user-select: none;
+        -webkit-tap-highlight-color: rgba(0, 0, 0, 0);
+      }
+
+      [anim="ripple"] {
+        position: relative;
+        overflow: hidden;
+      }
+
+      .ripple:before {
+        content: "";
+        position: absolute;
+        display: block;
+        background: var(--ripple-background, white);
+        border-radius: 50%;
+        pointer-events: none;
+
+        //  position and size
+        top: calc(var(--y) * 1px);
+        left: calc(var(--x) * 1px);
+        width: calc(var(--d) * 1px);
+        height: calc(var(--d) * 1px);
+
+        //  animated properties
+        opacity: calc(var(--o, 1) * var(--ripple-opacity, 0.3));
+        transition: calc(var(--t, 0) * var(--ripple-duration, 600ms))
+          var(--ripple-easing, linear);
+        transform: translate(-50%, -50%) scale(var(--s, 1));
+        transform-origin: center;
       }
     `;
   }
@@ -89,6 +159,8 @@ export class AutoclickerGame extends LitElement {
       autoclickerBaseCost: { type: Number },
       autoclikerCost: { type: Number },
       interval: { type: Array },
+      boosted: { type: Boolean, reflect: true },
+      planet: { type: String },
     };
   }
 
@@ -99,6 +171,15 @@ export class AutoclickerGame extends LitElement {
     this.autoclickerBaseCost = 1;
     this.autoclikerCost = 50;
     this.interval = [];
+    this.boosted = false;
+    this.planets = [
+      "../assets/planets/jupiter.png",
+      "../assets/planets/mars.png",
+      "../assets/planets/planet-earth.png",
+      "../assets/planets/saturn.png",
+    ];
+
+    this.planet = "";
   }
 
   firstUpdated() {
@@ -106,6 +187,9 @@ export class AutoclickerGame extends LitElement {
     this.counter = JSON.parse(localStorage.getItem("users")).find(
       (user) => user.name === this.user.name
     ).points;
+
+    this.planet = this.planets[Math.floor(Math.random() * this.planets.length)];
+    this.rippleBtn = this.shadowRoot.getElementById("ripple-btn");
 
     if (this.user.baseCost > 1) {
       this.autoclickerBaseCost = this.user.baseCost;
@@ -117,7 +201,7 @@ export class AutoclickerGame extends LitElement {
   render() {
     return html`
       <header>
-        <h1>Autoclicker</h1>
+        <h1>PlanetClicker</h1>
         <iron-icon
           class="back"
           icon="icons:exit-to-app"
@@ -126,12 +210,27 @@ export class AutoclickerGame extends LitElement {
       </header>
       <section>
         <h2>Hi ${this.user.name}</h2>
-        <p>Points: ${this.counter}</p>
-        <p>Autoclickers bought: ${this.autoclickerBaseCost - 1}</p>
-        <button class="active" @click=${this.addClick}>Click</button>
+        <p>Planets discovered: ${this.counter}</p>
+        <img src="${this.planet}" alt="planet icon" class="planet" />
+        <div class="energy">
+          <img
+            src="https://images.vexels.com/media/users/3/143495/isolated/preview/6b80b9965b1ec4d47c31d7eccf8ce4b0-icono-de-rayo-amarillo.png"
+            alt="energy icon"
+          />
+          <p>Energy boosted: ${this.autoclickerBaseCost - 1}</p>
+        </div>
+
+        <button
+          @click=${this.addClick}
+          id="ripple-btn"
+          anim="ripple"
+          class="ripple"
+        >
+          Discover planet
+        </button>
         ${this.autoclickerBaseCost > 1 || this.counter >= 50
           ? html`<button
-              @click=${this.buyAutoclicker}
+              @click=${(e) => this.buyAutoclicker(e)}
               ?disabled=${this.counter < this.autoclikerCost ? true : false}
               class=${this.counter < this.autoclikerCost
                 ? "disabled"
@@ -157,8 +256,9 @@ export class AutoclickerGame extends LitElement {
     );
   }
 
-  addClick() {
+  addClick(e) {
     this.counter += 1;
+    this.rippleEffect(e);
     this.updateUser();
   }
 
@@ -167,10 +267,11 @@ export class AutoclickerGame extends LitElement {
     this.autoclickerBaseCost = this.user.baseCost + 1;
     this.autoclikerCost = this.autoclikerCost + 50 * this.autoclickerBaseCost;
     this.addAutoclickerInterval(this.autoclickerBaseCost);
+    this.boostedEffect();
   }
 
   addAutoclickerInterval(basecost) {
-    const timeInterval = basecost > 1 ? 100 / basecost : 100;
+    const timeInterval = basecost > 2 ? 100 / basecost : 100;
 
     const currentInterval = setInterval(
       this.autoclickerEffect.bind(this),
@@ -178,6 +279,13 @@ export class AutoclickerGame extends LitElement {
     );
 
     this.interval.push(currentInterval);
+  }
+
+  boostedEffect() {
+    this.boosted = true;
+    setTimeout(() => {
+      this.boosted = false;
+    }, 500);
   }
 
   autoclickerEffect() {
@@ -189,21 +297,33 @@ export class AutoclickerGame extends LitElement {
   updateUser() {
     let allUsers = JSON.parse(localStorage.getItem("users"));
 
+    const userUpdated = {
+      ...this.user,
+      points: this.counter,
+      baseCost: this.autoclickerBaseCost,
+      clickerCost: this.autoclikerCost,
+    };
+
     allUsers = allUsers.map((user) =>
-      user.name === this.user.name
-        ? {
-            ...user,
-            points: this.counter,
-            baseCost: this.autoclickerBaseCost,
-            clickerCost: this.autoclikerCost,
-          }
-        : user
+      user.name === this.user.name ? userUpdated : user
     );
 
     localStorage.setItem("users", JSON.stringify(allUsers));
+    localStorage.setItem("lastUser", JSON.stringify(userUpdated));
 
     this.user = JSON.parse(localStorage.getItem("users")).find(
       (user) => user.name === this.user.name
     );
+  }
+
+  rippleEffect(e) {
+    e = e.touches ? e.touches[0] : e;
+    const r = this.rippleBtn.getBoundingClientRect();
+    const d = Math.sqrt(Math.pow(r.width, 2) + Math.pow(r.height, 2)) * 2;
+    this.rippleBtn.style.cssText = `--s: 0; --o: 1;`;
+    this.rippleBtn.offsetTop;
+    this.rippleBtn.style.cssText = `--t: 1; --o: 0; --d: ${d}; --x:${
+      e.clientX - r.left
+    }; --y:${e.clientY - r.top};`;
   }
 }
