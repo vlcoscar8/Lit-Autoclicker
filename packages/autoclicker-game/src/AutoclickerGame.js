93712f6b08a4f953471c88be5660f9a06597fbd6
Perez Romero <operezr@minsait.com>
2022-10-10 20:08:50 +0200

    feat(autoclicker-game): refactor styles and separete in other file

--- packages/autoclicker-game/src/AutoclickerGame.js
+++ packages/autoclicker-game/src/AutoclickerGame.js
@@ -1,212 +1,12 @@
 import { html, css, LitElement } from "lit";
+import { gameStyles } from "./game.styles";
 
 import "@polymer/iron-icons";
 
 export class AutoclickerGame extends LitElement {
   static get styles() {
     return css`
-      :host {
-        width: 100vw;
-        height: 100%;
-
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-        justify-content: flex-start;
-        gap: 2rem;
-
-        overflow: hidden;
-      }
-
-      header {
-        display: flex;
-        align-items: center;
-        justify-content: space-between;
-        width: 100%;
-        background-color: var(--game-header-background-color, #ccc);
-      }
-
-      h1 {
-        font-size: 2.5rem;
-        background: var(--app-title-color-primary, white);
-        background-clip: none;
-        -webkit-background-clip: text;
-        -webkit-text-fill-color: transparent;
-        margin: 2rem;
-      }
-
-      .back {
-        color: var(--game-header-icon-color, white);
-        transform: scale(1.5);
-        margin: 2rem;
-      }
-
-      section {
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-        justify-content: flex-start;
-      }
-
-      .name {
-        display: flex;
-        align-items: center;
-        gap: 1rem;
-      }
-
-      .name img {
-        width: 5rem;
-      }
-
-      h2 {
-        font-size: 2.5rem;
-        font-weight: 100;
-        color: var(--home-title-color, white);
-      }
-
-      p {
-        font-size: 2rem;
-        font-weight: 100;
-        color: var(--home-title-color, white);
-      }
-
-      button {
-        margin-top: 1.5rem;
-        font-size: 1.5rem;
-        padding: 1.5rem 9rem;
-        border-radius: 0.7rem;
-        text-transform: uppercase;
-        color: var(--home-button-text-color, #000000);
-        border: 1px solid var(--home-button-border-color, #000000);
-        background-color: var(--home-button-background-color, #86edff);
-      }
-
-      .disabled {
-        color: var(--game-button-disabled-text-color, #000000);
-        border: 1px solid var(--game-button-disabled-border-color, #000000);
-        background-color: var(--game-button-disabled-background-color, #86edff);
-      }
-
-      .active {
-        color: var(--home-button-text-color, #000000);
-        border: 1px solid var(--home-button-border-color, #000000);
-        background-color: var(--game-button-active-background-color, #86edff);
-      }
-
-      .energy {
-        display: flex;
-        align-items: center;
-        justify-content: center;
-        box-sizing: border-box;
-        gap: 1rem;
-        width: 80vw;
-        height: 6rem;
-      }
-
-      .energy img {
-        width: 3rem;
-      }
-
-      :host([boosted]) .energy img {
-        animation: grow 0.5s ease;
-      }
-
-      .planet {
-        width: 7rem;
-      }
-
-      .factory {
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-        justify-content: flex-start;
-        gap: 1em;
-        margin-top: 3rem;
-        color: white;
-      }
-
-      .factory__header {
-        display: flex;
-        align-items: center;
-        justify-content: center;
-        gap: 1rem;
-        font-size: 1.7rem;
-      }
-
-      .factory-icon {
-        transform: rotate(180deg) scale(0.8);
-      }
-
-      .factory img {
-        width: 4rem;
-      }
-
-      .factory button {
-        display: flex;
-        align-items: center;
-        justify-content: space-between;
-        gap: 1rem;
-        width: 90vw;
-        padding: 1rem 3rem;
-      }
-
-      .rocket-active {
-        color: var(--home-button-text-color, #000000);
-        border: 1px solid var(--home-button-border-color, #000000);
-        background-color: var(
-          --game-button-rocket-active-background-color,
-          #86edff
-        );
-      }
-
-      :host([factoryView]) .factory-icon {
-        transform: rotate(0deg) scale(0.8);
-      }
-
-      @keyframes grow {
-        0% {
-          width: 3rem;
-        }
-
-        50% {
-          width: 4rem;
-        }
-      }
-
-      .ripple {
-        --ripple-background: #992d2d4c;
-        --ripple-opacity: 0.1;
-        --ripple-duration: 600ms;
-        user-select: none;
-        -webkit-tap-highlight-color: rgba(0, 0, 0, 0);
-      }
-
-      [anim="ripple"] {
-        position: relative;
-        overflow: hidden;
-      }
-
-      .ripple:before {
-        content: "";
-        position: absolute;
-        display: block;
-        background: var(--ripple-background, white);
-        border-radius: 50%;
-        pointer-events: none;
-
-        //  position and size
-        top: calc(var(--y) * 1px);
-        left: calc(var(--x) * 1px);
-        width: calc(var(--d) * 1px);
-        height: calc(var(--d) * 1px);
-
-        //  animated properties
-        opacity: calc(var(--o, 1) * var(--ripple-opacity, 0.3));
-        transition: calc(var(--t, 0) * var(--ripple-duration, 600ms))
-          var(--ripple-easing, linear);
-        transform: translate(-50%, -50%) scale(var(--s, 1));
-        transform-origin: center;
-      }
+      ${gameStyles()}
     `;
   }
 
@@ -259,7 +59,11 @@ export class AutoclickerGame extends LitElement {
     if (this.user.baseCost > 1) {
       this.autoclickerBaseCost = this.user.baseCost;
       this.autoclikerCost = this.user.clickerCost;
-      this.addAutoclickerInterval(this.autoclickerBaseCost);
+      this.addAutoclickerInterval(
+        this.user.rockets.basic.owned
+          ? this.autoclickerBaseCost
+          : this.autoclickerBaseCost * 2
+      );
     }
   }
 
@@ -377,7 +181,7 @@ export class AutoclickerGame extends LitElement {
       timeInterval
     );
 
-    this.interval[0] = currentInterval;
+    this.interval.push(currentInterval);
   }
 
   boostedEffect() {
